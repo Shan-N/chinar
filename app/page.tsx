70368a699cb41d516799303f78563e253c6d34a2
Shan-N <shantanu.neve2000@gmail.com>
2025-08-09 18:04:13 +0530

    below hero

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,12 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
-
+  const [selectedService, setSelectedService] = useState("EVENT");
   return (
     <div className="bg-white">
       <Navbar isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!isOpen && (
+      <>
       <main className="flex flex-col justify-center items-center py-8">
         <div className="relative px-4 rounded-lg container">
           <div className="relative w-full h-[400px] rounded-lg overflow-hidden">
@@ -43,10 +45,31 @@ export default function Home() {
             <motion.h2 className="text-white text-sm font-normal mt-2 sm:text-base ">
               India&apos;s #1 Wedding Planning &amp; Kashmiri Culinary Destination
             </motion.h2>
-            
           </div>
         </div>
       </main>
+      <section className={twMerge("px-4 md:px-32 flex flex-col gap-4 py-4", inter.className)}>
+          <h2 className="font-bold text-[#bd0a0ac9] text-xl md:text-2xl">About Us</h2>
+          <p className="text-sm md:text-normal">Since 2012, Chinar Hospitality is a Trusted name in crafting awesome weddings. As seasoned wedding planners, we are popular and specialized in Kashmiri Pandit Weddings. We blend traditional rituals with absolute modern elegance. We at Chinar aim to serve our clients with the utmost dedication and creativity. Our Dine-In experience is designed to offer a unique blend of traditional Kashmiri cuisine and modern culinary techniques, ensuring an unforgettable dining experience for our guests.</p>
+      </section>
+      <section className={twMerge("px-4 md:px-32 flex flex-col gap-4 py-4", inter.className)}>
+        <h2 className="font-bold text-[#bd0a0ac9] text-xl md:text-2xl">Our Services</h2>
+        <p className="text-sm md:text-normal">We offer a wide range of services to make your wedding day special, including venue selection, catering, decoration, and event management. Our team of experts will work with you to create a customized plan that fits your vision and budget.</p>
+        <div className="flex flex-row gap-4 border-b-2 border-[#bd0a0ac9]/20">
+          <span className={twMerge("font-bold text-neutral-900", selectedService === "EVENT" && "border-b-3 border-[#bd0a0ac9]")} onClick={() => setSelectedService("EVENT")}>Event Management</span>
+          <span className={twMerge("font-bold text-neutral-900", selectedService === "DINE" && "border-b-3 border-[#bd0a0ac9]")} onClick={() => setSelectedService("DINE")}>Dine-In</span>
+        </div>
+        <div>
+          {selectedService === "EVENT" && (
+            <p className="text-sm md:text-normal">Event Management Details...</p>
+          )}
+          {selectedService === "DINE" && (
+            <p className="text-sm md:text-normal">Dine-In Details...</p>
+          )}
+        </div>
+      </section>
+      </>
+      )}
     </div>
   );
 }
