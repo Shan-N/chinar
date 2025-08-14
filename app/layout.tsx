3e873d3ad1252c15eb9490f7cd80caf1419da95c
Shan-N <shantanu.neve2000@gmail.com>
2025-08-14 22:33:39 +0530

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 
 export const metadata: Metadata = {
@@ -60,6 +61,7 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
         />
+        <Analytics />
       </head>
       <body
         className={`bg-white antialiased`}
